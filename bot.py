87941afe9868851d637646d6a139fc7c6fbb4494
Thisal <93039058+Thisalx-x@users.noreply.github.com>
2022-01-29 11:15:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
-Print	(bot)
+'''imports'''
+import smtplib
+import sys
+
+
+class bcolors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+
+
+def banner():
+    print(bcolors.GREEN + '+[+[+[ Email-Bomber v1.0 ]+]+]+')
+    print(bcolors.GREEN + '+[+[+[ made with codes ]+]+]+')
+    print(bcolors.GREEN + ''' 
+
+             ███████╗███╗   ███╗ █████╗ ██╗██╗     
+             ██╔════╝████╗ ████║██╔══██╗██║██║     
+             █████╗  ██╔████╔██║███████║██║██║     
+             ██╔══╝  ██║╚██╔╝██║██╔══██║██║██║     
+             ███████╗██║ ╚═╝ ██║██║  ██║██║███████
+             ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝  ''')
